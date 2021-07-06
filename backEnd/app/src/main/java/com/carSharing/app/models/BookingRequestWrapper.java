7a3115c46a4fd55dce987b9b0de7c497b13671ed
rock-007 <umair.ashraf2019@gmail.com>
2021-07-06 11:01:17 +0100

    added Booking RequestWrapper class to spring

--- backEnd/app/src/main/java/com/carSharing/app/models/BookingRequestWrapper.java
+++ backEnd/app/src/main/java/com/carSharing/app/models/BookingRequestWrapper.java
@@ -1,2 +1,27 @@
-package com.carSharing.app.models;public class BookingRequestWrapper {
+package com.carSharing.app.models;
+
+public class BookingRequestWrapper {
+    Availability availability;
+    User requestingUser;
+
+    public BookingRequestWrapper(Availability availability, User requestingUser) {
+        this.availability = availability;
+        this.requestingUser = requestingUser;
+    }
+
+    public Availability getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Availability availability) {
+        this.availability = availability;
+    }
+
+    public User getRequestingUser() {
+        return requestingUser;
+    }
+
+    public void setRequestingUser(User requestingUser) {
+        this.requestingUser = requestingUser;
+    }
 }
